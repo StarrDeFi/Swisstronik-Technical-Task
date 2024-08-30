6d5eac9a1c1061c9058d76261ccd0295fc6354b0
StarrDeFi <estheressienjames2020@gmail.com>
2024-08-30 12:25:18 +0100

    Create StarrDeFi.sh

---
+++ StarrDeFi.sh
@@ -0,0 +1,7 @@
+echo "# Swisstronik-Technical-Task" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/StarrDeFi/Swisstronik-Technical-Task.git
+git push -u origin main
